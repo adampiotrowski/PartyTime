d49f0ab96fd7d30b9dd799e2163c07309f9fdf4f
Adam Piotrowski <adam@wellcommerce.org>
2017-11-21 00:42:34 +0100

    Test

--- tests/AppBundle/Controller/PartyControllerTest.php
+++ tests/AppBundle/Controller/PartyControllerTest.php
@@ -4,15 +4,23 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class PartyControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
-
+        
         $crawler = $client->request('GET', '/');
-
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Parties list', $crawler->filter('#container h1')->text());
+    }
+    
+    public function testSearch()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/search/51.80,19.37');
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
